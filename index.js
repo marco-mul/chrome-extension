ad6f149cb393d87af9a43c59983f081a70a3b2f9
Marco Muller <marco.mul.dev@gmail.com>
2026-07-13 17:00:30 +0200

    add quote

--- index.js
+++ index.js
@@ -4,13 +4,28 @@ fetch(
   .then((res) => res.json())
   .then((data) => {
     document.body.style.backgroundImage = `url(${data.urls.regular})`;
-    document.getElementById("author").textContent = `By: ${data.user.name}`;
+    document.getElementById("author").textContent = `📸 ${data.user.name}`;
   })
   .catch((err) => {
     // Use a default background image/author
     document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
 )`;
     document.getElementById("author").textContent = `By: Dodi Achmad`;
+  });
+
+fetch("https://api.adviceslip.com/advice")
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+    document.getElementById("quote").innerHTML = `
+    <span class="quote-title">Some Words of Wisdom</span>
+    <p>${data.slip.advice}</p>
+    `;
+  })
+  .catch((err) => {
+    document.getElementById("quote").innerHTML = `
+    <span class="quote-title">Some Words of Wisdom</span>
+    <p>Take a short break and stretch.</p>`;
   });
 
 fetch("https://api.coingecko.com/api/v3/coins/bitcoin")
