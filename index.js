59c3c7502c6b972a104d6d1b9d98fb230b509de7
Marco Muller <marco.mul.dev@gmail.com>
2026-07-14 16:17:37 +0200

    add newsfeed

--- index.js
+++ index.js
@@ -13,22 +13,24 @@ fetch(
     document.getElementById("author").textContent = `By: Dodi Achmad`;
   });
 
-fetch("https://icanhazdadjoke.com/", {
-  headers: { Accept: "application/json" },
-})
-  .then((res) => res.json())
-  .then((data) => {
-    console.log(data);
-    document.getElementById("quote").innerHTML = `
+setTimeout(() => {
+  fetch("https://icanhazdadjoke.com/", {
+    headers: { Accept: "application/json" },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log("joke: ", data);
+      document.getElementById("quote").innerHTML = `
     <span class="quote-title">You're in for a joke</span>
     <p>${data.joke.charAt(0).toUpperCase() + data.joke.slice(1)}</p>
     `;
-  })
-  .catch((err) => {
-    document.getElementById("quote").innerHTML = `
+    })
+    .catch((err) => {
+      document.getElementById("quote").innerHTML = `
     <span class="quote-title">You're in for a joke</span>
     <p>Or are you?</p>`;
-  });
+    });
+}, 3000);
 
 fetch("https://api.frankfurter.dev/v2/rates?base=EUR&quotes=USD,GBP,CHF")
   .then((res) => {
@@ -38,7 +40,7 @@ fetch("https://api.frankfurter.dev/v2/rates?base=EUR&quotes=USD,GBP,CHF")
     return res.json();
   })
   .then((data) => {
-    console.log(data);
+    console.log("currencies: ", data);
     document.getElementById("crypto-top").innerHTML = `
             <img src="currency-compare.svg" alt="EUR vs USD, GBP, CHF" />         
         `;
@@ -48,6 +50,33 @@ fetch("https://api.frankfurter.dev/v2/rates?base=EUR&quotes=USD,GBP,CHF")
             <p>${data[1].quote}: ${data[1].rate}</p>
             <p>${data[0].quote}: ${data[0].rate}</p>
         `;
+  })
+  .catch((err) => console.error(err));
+
+fetch(
+  `https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${import.meta.env.VITE_NYT_API_KEY}`,
+)
+  .then((res) => {
+    if (!res.ok) {
+      throw Error("Something went wrong");
+    }
+    return res.json();
+  })
+  .then((data) => {
+    console.log("articles: ",data);
+    function renderStory() {
+      const randomIndex = Math.floor(Math.random() * data.results.length);
+      document.getElementById("story").innerHTML = `
+            <h2>${data.results[randomIndex].title}</h2>
+            <p>${data.results[randomIndex].abstract}</p>
+            <button id="story-link">Read more on The New York Times</button>
+        `;
+      document.getElementById("story-link").addEventListener("click", () => {
+        window.open(data.results[randomIndex].url, "_blank");
+      });
+    }
+    renderStory();
+    setInterval(renderStory, 15000);
   })
   .catch((err) => console.error(err));
 
@@ -61,23 +90,26 @@ function getCurrentTime() {
 
 setInterval(getCurrentTime, 1000);
 
-navigator.geolocation.getCurrentPosition((position) => {
-  fetch(
-    `https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric`,
-  )
-    .then((res) => {
-      if (!res.ok) {
-        throw Error("Weather data not available");
-      }
-      return res.json();
-    })
-    .then((data) => {
-      const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-      document.getElementById("weather").innerHTML = `
+navigator.geolocation.getCurrentPosition(
+  (position) => {
+    fetch(
+      `https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric`,
+    )
+      .then((res) => {
+        if (!res.ok) {
+          throw Error("Weather data not available");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+        document.getElementById("weather").innerHTML = `
                 <img src=${iconUrl} />
                 <p class="weather-temp">${Math.round(data.main.temp)}ºC</p>
                 <p class="weather-city">${data.name}</p>
             `;
-    })
-    .catch((err) => console.error(err));
-});
+      })
+      .catch((err) => console.error(err));
+  },
+  (err) => console.error("Geolocation failed:", err.code, err.message),
+);
